1abb4b04154dab37c362a7809cdc3622cbeb9e6d
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-06-25 01:35:42 +0530

    ADD. Admin Authentication contorller were added.

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const { check } = require('express-validator')
 const { login } = require('../controller/authController')
+const { verifyUser, verifyAdmin } = require('../middleware/verify')
+const { authenticateAdmin } = require('../controller/adminController')
 
 router.post(
     '/login',
@@ -17,6 +19,15 @@ router.post(
         .withMessage('Password length is at least 8 character'),
 
     login
+)
+
+router.get(
+    '/authenticate',
+
+    verifyUser,
+    verifyAdmin,
+
+    authenticateAdmin
 )
 
 module.exports = router
