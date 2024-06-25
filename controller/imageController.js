25634ef6c7459f19b614e8be3c12b6962fdb92a0
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-06-25 09:50:54 +0530

    UPDATE. Update the image route to send the current directory of the controller.

--- controller/imageController.js
+++ controller/imageController.js
@@ -2,7 +2,7 @@ const getFileByName = (request, response) => {
     const {filename} = request.params
     const parentDirectory = (__dirname).split('controller')[0]
     const filePath = parentDirectory + 'public/images/' + filename
-    response.send(filePath)
+    response.send(__dirname)
 }
 
 module.exports = {
